ace12d37e4e27f471ca96c7a59d8d160fd056198
Prashant Aggarwal <pxaggar@gmail.com>
2024-05-12 17:52:30 -0400

    new code

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-provider "aws" {
-    region = "us-east-1"  
-}
+resource "aws_instance" "public_instance" {
+ ami           = var.ami
+ instance_type = var.instance_type
 
-resource "aws_instance" "foo" {
-  ami           = "ami-0e8ad69da124219b7" # Susi Linux
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
+ tags = {
+   Name = var.name_tag,
+ }
 }
