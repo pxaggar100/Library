20f3109880bbcedd900b81f98c0ec4988be82fa0
Prashant Aggarwal <pxaggar@gmail.com>
2024-05-14 11:37:22 -0400

    ec2

---
+++ aws/awsEC2/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-07caf09b362be10b8"
+  instance_type   = "t2.micro"
+  security_groups = ["launch-wizard-1"]
+  key_name        = "ps1"
+  tags = {
+    Name          = "AWEDEVPMTA059785"
+    Environment   = "PMT-Dev"
+    AppID         = "A059785"
+    AppName       = "Payment System"
+    AppOwner      = "Adam First name Last name "
+    Email         = "Adam@bankofabcd.com"
+    SLA           = "Platinum"
+    Criticality   = "Tier1"
+    Availability  = "Highest"
+    ProvisionedBy = "tom.cook@icasfas.com"
+  }
+}
